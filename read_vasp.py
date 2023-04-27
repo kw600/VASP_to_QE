938c4d9ca4291272ae963af4305e56e63c2427ff
Kang Wang <kw600@cam.ac.uk>
2023-04-27 11:43:06 +0100

    add sub

--- read_vasp.py
+++ read_vasp.py
@@ -23,8 +23,29 @@ def vasp_to_qe(path):
 				}
 
 	kpoints = (4,4,4) # The k-points mesh
-	atoms.write('input.pwi',input_data=input_params,pseudopotentials=pseudos,format='espresso-in',kpts=kpoints)
+	atoms.write('Si.pwi',input_data=input_params,pseudopotentials=pseudos,format='espresso-in',kpts=kpoints)
+
+def sub():
+	input='''#!/bin/bash
+# Slurm job options (job-name, compute nodes, job time)
+#SBATCH --nodes=4
+#SBATCH --tasks-per-node=128
+#SBATCH --job-name=qscaild
+#SBATCH --account=e89-ic_m
+#SBATCH --partition=standard
+#SBATCH --qos=taskfarm
+#SBATCH --time=04:00:00
+
+export OMP_NUM_THREADS=1
+
+module load quantum_espresso
+
+srun --distribution=block:block --hint=nomultithread pw.x < Si.pwi > Si.pwo
+'''
+	with open('sub_qscaild','w') as f:
+		f.write(input)
 
 if __name__=='__main__':
 	path = sys.argv[1] # path to the directory containing the POSCAR file
 	vasp_to_qe(path)
+	sub()
